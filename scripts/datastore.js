168f47d339e9bb67c7ccbe653bb1338b0334f801
Lukas Singer <7627101@isd535.org>
2022-03-10 10:41:20 -0600

    Made data in DataStore private

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -1,25 +1,26 @@
 (function (window) {
   "use strict";
   let App = window.App || {};
+  let data;
 
   function DataStore() {
-    this.data = {};
+    data = {};
   }
 
   DataStore.prototype.add = function (key, val) {
-    this.data[key] = val;
+    data[key] = val;
   };
 
   DataStore.prototype.get = function (key) {
-    return this.data[key];
+    return data[key];
   };
 
   DataStore.prototype.getAll = function () {
-    return this.data;
+    return data;
   };
 
   DataStore.prototype.remove = function (key) {
-    delete this.data[key];
+    delete data[key];
   };
 
   App.DataStore = DataStore;
