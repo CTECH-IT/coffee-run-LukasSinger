9dfe800276aa9fbed9fbe72b9935d6d279ae060f
Lukas Singer <7627101@isd535.org>
2022-03-17 11:05:53 -0500

    Added supermodal UI

--- scripts/formHandler.js
+++ scripts/formHandler.js
@@ -2,11 +2,12 @@
   let App = window.App || {};
   let $ = window.jQuery;
 
-  function FormHandler(formSelector, resetSelector, slider) {
-    if (!formSelector || !resetSelector)
+  function FormHandler(formSelector, resetSelector, superSelector, slider) {
+    if (!formSelector || !resetSelector || !superSelector)
       throw new Error("Oops! You forgot to provide a selector!");
     this.$formElement = $(formSelector);
     this.$resetElement = $(resetSelector);
+    this.$superModal = $(superModal);
     this.slider = slider;
     if (this.$formElement.length == 0) {
       throw new Error(
@@ -15,6 +16,10 @@
     } else if (this.$resetElement.length == 0) {
       throw new Error(
         "Typo? Could not find element with selector: " + resetSelector
+      );
+    } else if (this.$superModal.length == 0) {
+      throw new Error(
+        "Typo? Could not find element with selector: " + superSelector
       );
     }
   }
@@ -31,6 +36,9 @@
       func(data);
       this.$formElement[0].reset();
       this.$formElement[0].elements[0].focus();
+      if (data["size"] == "yuge" && data["purity"] == 100) {
+        this.$superModal.modal();
+      }
     });
   };
 
--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@
   const FORM_SELECTOR = '[data-chocolate-order="form"]';
   const SLIDER_SELECTOR = '[data-chocolate-order="slider"]';
   const SLIDER_LABEL_SELECTOR = '[data-chocolate-order="sliderLabel"]';
+  const POWERUP_MODAL_SELECTOR = "#superModal";
   const RESET_BUTTON_SELECTOR = 'button[type="reset"]';
 
   let App = window.App;
@@ -25,6 +26,7 @@
   let formHandler = new FormHandler(
     FORM_SELECTOR,
     RESET_BUTTON_SELECTOR,
+    POWERUP_MODAL_SELECTOR,
     slider
   );
   formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
